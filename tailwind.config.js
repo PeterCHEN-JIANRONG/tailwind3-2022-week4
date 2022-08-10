228f6ea9b6122f16cc13af526794041890dadeff
peter.chen <peter@begonia-design.com.tw>
2022-08-11 00:19:27 +0800

    add recommend style

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,13 @@ module.exports = {
           dark: '#650300',
         },
         light: '#FBF2F2',
+        info: {
+          light: '#DEE2E6',
+          DEFAULT: '#707070',
+        },
       },
       fontSize: {
+        sm: ['14px', '1.5'],
         base: ['16px', '1.5'],
         lg: ['18px', '1.5'],
         xl: ['20px', '1.5'],
@@ -23,7 +28,11 @@ module.exports = {
         '4xl': ['36px', '1.5'],
         '5xl': ['48px', '1.5'],
         '6xl': ['64px', '1.5'],
-      }
+      },
+      boxShadow: {
+        'card': '0px 2px 6px #00000029',
+        'card-md': '4px 6px 6px #00000029',
+      },
     },
   },
   plugins: [
